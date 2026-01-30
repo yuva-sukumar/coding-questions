6b0a54ea059844d785f9fd6611f5335bc4bf6141
yuvarakshanas <yuvarakshana.sukumar@sky.uk>
2026-01-30 18:02:37 +0530

    Correctly add submodule

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: "https://yuva-sukumar.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/yuva-sukumar/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
